7cb466336f68ea930229bda2c478ec98c335062e
Corey Davis <coreyd303@gmail.com>
2014-08-05 11:31:09 -0600

    complete I:4 add some gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ActiveRecord::Base
-  has_many :comments
-  has_many :taggings
-  has_many :tags, through: :taggings
+  has_many                          :comments
+  has_many                          :taggings
+  has_many                          :tags, through: :taggings
+  has_attached_file                 :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def tag_list
     self.tags.collect do |tag|
@@ -10,12 +12,8 @@ class Article < ActiveRecord::Base
   end             # interesting code...
 
   def tag_list=(tags_string)
-    # split the tags string into an array of strings with leading and trailing white space
-    # then, ensure that each one is unique
     tag_names = tags_string.split(",").collect{ |s| s.strip.downcase }.uniq
-    # look for at tag object with that name, if there isn't one make it
     new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
-    # add the tag object to a list of tags  for the article
     self.tags = new_or_found_tags
   end
 end
